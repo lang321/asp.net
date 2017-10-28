25525e95291bbba1914702ec3d7d296ccaca6478
zhu95 <zhu952703603@outlook.com>
2017-10-28 18:12:51 +0800

    write a line

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            Console.WriteLine( "i use visual studio 2015");
         }
     }
 }
